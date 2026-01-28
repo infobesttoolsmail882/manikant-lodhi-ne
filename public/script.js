ccdf8a5f97f6bd83ce309782d1559a99fc202c29
infobesttoolsmail882 <info@besttoolsmail.online>
2026-01-28 15:39:41 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 function updateCounter() {
-  const recipients = document.getElementById("recipients").value;
+  const recipients = document.getElementById("to").value;
   const count = recipients.split(/,|\n/).filter(r => r.trim()).length;
-  document.getElementById("counter").innerText = `0/${count}`;
+  document.getElementById("limitText").innerText = `0/${count}`;
   return true;
 }
