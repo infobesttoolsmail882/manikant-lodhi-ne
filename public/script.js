fcbd0f5242c65be41c2c64ea0c48fba43dd3d8e3
infobesttoolsmail882 <info@besttoolsmail.online>
2026-01-28 09:54:59 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,29 +1,20 @@
-const sendBtn = document.getElementById('sendBtn');
-
-sendBtn.onclick = async () => {
-  sendBtn.disabled = true;
-  sendBtn.innerText = "Sending…";
-
-  const res = await fetch('/send', {
-    method:'POST',
-    headers:{'Content-Type':'application/json'},
+document.getElementById("sendBtn").onclick = async () => {
+  const res = await fetch("/send", {
+    method:"POST",
+    headers:{ "Content-Type":"application/json" },
     body:JSON.stringify({
-      senderName: senderName.value,
       email: email.value,
       password: password.value,
+      to: to.value,
       subject: subject.value,
-      message: message.value,
-      recipients: recipients.value
+      message: message.value
     })
   });
 
   const data = await res.json();
   alert(data.message);
-
-  sendBtn.disabled = false;
-  sendBtn.innerText = "Send";
 };
 
 function logout(){
-  fetch('/logout',{method:'POST'}).then(()=>location.href='/login');
+  fetch("/logout",{method:"POST"}).then(()=>location.href="/login");
 }
