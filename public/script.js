bd51d80d331e7bb459223c6d472c3d2618b107ee
infobesttoolsmail882 <info@besttoolsmail.online>
2026-01-28 15:13:20 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
-document.querySelector("form").addEventListener("submit", function () {
-  const recipients = document.querySelector("textarea[name='recipients']").value;
+function updateCounter() {
+  const recipients = document.getElementById("recipients").value;
   const count = recipients.split(/,|\n/).filter(r => r.trim()).length;
   document.getElementById("counter").innerText = `0/${count}`;
-});
+  return true;
+}
