ebada382eb8112f8074e0de5c557381474027075
infobesttoolsmail882 <info@besttoolsmail.online>
2026-02-01 14:30:15 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,22 +1,17 @@
-const sendBtn = document.getElementById("sendBtn");
-const logoutBtn = document.getElementById("logoutBtn");
-
-logoutBtn.addEventListener("dblclick", async () => {
-  await fetch("/logout", { method: "POST" });
-  location.href = "/login.html";
+logoutBtn.addEventListener("dblclick", async ()=>{
+  await fetch("/logout",{method:"POST"});
+  location.href="/login.html";
 });
 
-sendBtn.addEventListener("click", async () => {
-  sendBtn.disabled = true;
-  sendBtn.innerText = "Sending…";
+sendBtn.addEventListener("click", async ()=>{
+  sendBtn.disabled=true;
+  sendBtn.innerText="Sending…";
 
-  const res = await fetch("/send", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+  const res = await fetch("/send",{
+    method:"POST",
+    headers:{ "Content-Type":"application/json" },
     body: JSON.stringify({
       senderName: senderName.value,
-      gmail: gmail.value,
-      apppass: apppass.value,
       subject: subject.value,
       message: message.value,
       to: to.value
@@ -24,18 +19,15 @@ sendBtn.addEventListener("click", async () => {
   });
 
   const data = await res.json();
-
-  sendBtn.disabled = false;
-  sendBtn.innerText = "Send";
-
-  showPopup(data.msg || "Done");
+  showPopup(data.msg);
+  sendBtn.disabled=false;
+  sendBtn.innerText="Send";
 });
 
-function showPopup(msg) {
-  popupText.innerText = msg;
+function showPopup(msg){
+  popupText.innerText=msg;
   popup.classList.remove("hidden");
 }
-
-function closePopup() {
+function closePopup(){
   popup.classList.add("hidden");
 }
